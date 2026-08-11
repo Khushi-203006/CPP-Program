22cd5e047c8c3b22c590ca5c9afc3242cc483254
Khushi Rajvanshi <khushirajvanshi203@gmail.com>
2026-08-11 16:57:14 +0530

    OOPS program

--- OOPS/oops.cpp
+++ OOPS/oops.cpp
@@ -10,10 +10,16 @@ class Teacher{
     public:
         // properties / attributes
         // non-parameterized constructor
-        
         Teacher(){
             dept = "CSE";
     }
+
+        Teacher(string name , string dept , string subject , double salary){
+            this->name = name;
+            this->dept = dept;
+            this->subject = subject;
+            this->salary = salary;
+        }
         string name;
         string dept;
         string subject;
@@ -23,8 +29,8 @@ class Teacher{
             dept = newdpt;
     }
     //setter
-    void salary_1(double s){
-        salary = s;
+    void salary_1(double salary){
+        this->salary = salary;
     }
     //getter
     double salary_2(){
@@ -49,14 +55,16 @@ class Account{
 };
 
 int main(){
-    Teacher t1;
-    t1.name = "Riya";
+    Teacher t1("Riya", "CSE", "OOPs", 150000);
+    // t1.name = "Riya";
     // t1.dept = "Data Science";
-    t1.subject = "OOPs";
-    t1.salary_1(150000);
+    // t1.subject = "OOPs";
+    // t1.salary_1(150000);
     // t1.salary = 50000; //now we cant access it
 
     cout << t1.name << endl;
     cout << t1.dept << endl;
+    cout << t1.subject << endl;
+    cout << t1.salary_2() << endl;
     return 0;    
 }
